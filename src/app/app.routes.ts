d82910947458b67e7a5b563027b85ed8f7bf816e
SeanOMahony <g00472952@atu.ie>
2026-05-01 22:16:29 +0100

    Add movie details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
+    path: 'movie-details/:id',
+    loadComponent: () => import('./movie-details/movie-details.page').then((m) => m.MovieDetailsPage),
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
---
+++ src/app/movie-details/movie-details.page.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButtons,
+  IonButton,
+  IonIcon,
+  IonCard,
+  IonCardContent
+} from '@ionic/angular/standalone';
+import { NgFor, NgIf } from '@angular/common';
+import { addIcons } from 'ionicons';
+import { home, heart, heartOutline } from 'ionicons/icons';
+import { TmdbService } from '../services/tmdb.service';
+import { FavouritesService } from '../services/favourites.service';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.page.html',
+  styleUrls: ['./movie-details.page.scss'],
+  standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButtons,
+    IonButton,
+    IonIcon,
+    IonCard,
+    IonCardContent,
+    NgFor,
+    NgIf
+  ]
+})
+export class MovieDetailsPage implements OnInit {
+  movie: any = null;
+  cast: any[] = [];
+  crew: any[] = [];
+  isFavourite: boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private tmdbService: TmdbService,
+    private favouritesService: FavouritesService
+  ) {
+    addIcons({ home, heart, heartOutline });
+  }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    if (id) {
+      this.loadMovie(id);
+      this.loadCredits(id);
+    }
+  }
+
+  loadMovie(id: number): void {
+    this.tmdbService.getMovieDetails(id).subscribe({
+      next: (response: any) => {
+        this.movie = response;
+        this.isFavourite = this.favouritesService.isFavourite(this.movie);
+      },
+      error: (error) => {
+        console.error('Movie details error:', error);
+      }
+    });
+  }
+
+  loadCredits(id: number): void {
+    this.tmdbService.getMovieCredits(id).subscribe({
+      next: (response: any) => {
+        this.cast = response.cast || [];
+        this.crew = response.crew || [];
+      },
+      error: (error) => {
+        console.error('Movie credits error:', error);
+      }
+    });
+  }
+
+  toggleFavourite(): void {
+    if (!this.movie) return;
+
+    this.favouritesService.toggleFavourite(this.movie);
+    this.isFavourite = this.favouritesService.isFavourite(this.movie);
+  }
+
+  goHome(): void {
+    this.router.navigate(['/home']);
+  }
+
+  openFavourites(): void {
+    this.router.navigate(['/favourites']);
+  }
+  openPersonDetails(personId: number): void {
+  this.router.navigate(['/person', personId]);
+}
+}
