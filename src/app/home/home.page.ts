212d1e6735917448c7b6d7680c24495c48fa026f
SeanOMahony <g00472952@atu.ie>
2026-05-02 23:11:28 +0100

    Add loading spinner component to home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,31 +18,35 @@ import { FormsModule } from '@angular/forms';
 import { addIcons } from 'ionicons';
 import { heart } from 'ionicons/icons';
 import { TmdbService } from '../services/tmdb.service';
+import { IonSpinner } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
- imports: [
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonButtons,
-  IonButton,
-  IonIcon,
-  IonItem,
-  IonInput,
-  IonCard,
-  IonCardContent,
-  NgFor,
-  NgIf,
-  FormsModule,
-  RouterLink
-],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButtons,
+    IonButton,
+    IonIcon,
+    IonItem,
+    IonInput,
+    IonCard,
+    IonCardContent,
+    IonSpinner, 
+    NgFor,
+    NgIf,
+    FormsModule,
+    RouterLink
+  ],
 })
 export class HomePage implements OnInit {
+
+  loading: boolean = true;
   movies: any[] = [];
   searchText: string = '';
   pageTitle: string = "Today's Trending Movies";
@@ -59,13 +63,17 @@ export class HomePage implements OnInit {
   }
 
   loadTrendingMovies(): void {
+    this.loading = true;
     this.pageTitle = "Today's Trending Movies";
+
     this.tmdbService.getTrendingMovies().subscribe({
       next: (response: any) => {
         this.movies = response.results || [];
+        this.loading = false;
       },
       error: (error) => {
         console.error('TMDB error:', error);
+        this.loading = false;
       }
     });
   }
@@ -78,25 +86,22 @@ export class HomePage implements OnInit {
       return;
     }
 
+    this.loading = true;
     this.pageTitle = `${query} Movies`;
 
     this.tmdbService.searchMovies(query).subscribe({
       next: (response: any) => {
         this.movies = response.results || [];
+        this.loading = false;
       },
       error: (error) => {
         console.error('Search error:', error);
+        this.loading = false;
       }
     });
   }
 
-  openMovieDetails(movieId: number): void {
-    console.log('Clicked movie id:', movieId);
-    this.router.navigate(['/movie', movieId]);
-  }
-
   openFavourites(): void {
-    console.log('heart clicked');
     this.router.navigate(['/favourites']);
   }
 }
