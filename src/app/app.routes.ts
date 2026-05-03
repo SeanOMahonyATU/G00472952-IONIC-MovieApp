892b1ff476626c0b955abfc5c05f349d8283c29d
SeanOMahony <g00472952@atu.ie>
2026-05-03 21:36:14 +0100

    Add loading spinner component to home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,30 @@
 import { Routes } from '@angular/router';
 
+//  app routes
 export const routes: Routes = [
   {
+    // Home page
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
+    // movie-details page
     path: 'movie-details/:id',
     loadComponent: () => import('./movie-details/movie-details.page').then((m) => m.MovieDetailsPage),
   },
  
 {
+  // Favourites page
   path: 'favourites',
   loadComponent: () => import('./favourites/favourites.page').then(m => m.FavouritesPage),
 },
 {
+  // Person-details page 
   path: 'person-details/:id',
   loadComponent: () => import('./person-details/person-details.page').then(m => m.PersonDetailsPage),
 },
   {
+    // Default route
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -1,44 +1,50 @@
+//import angular,HttpClient for API
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+// Make service available 
 @Injectable({
   providedIn: 'root'
 })
 export class TmdbService {
+  // API key for TMDB
   private apiKey = '22d80a296e73cd54e02b8bf48841119e';
+  // Base URL for TMDB API
   private baseUrl = 'https://api.themoviedb.org/3';
 
   constructor(private http: HttpClient) {}
 
+  // Get today trending movies
   getTrendingMovies(): Observable<any> {
     return this.http.get<any>(
       `${this.baseUrl}/trending/movie/day?api_key=${this.apiKey}`
     );
   }
-
+// Search movies by name
   searchMovies(query: string): Observable<any> {
     return this.http.get<any>(
       `${this.baseUrl}/search/movie?query=${encodeURIComponent(query)}&api_key=${this.apiKey}`
     );
   }
 
-  // ➕ Add these methods
+  // details for a movie
   getMovieDetails(movieId: number): Observable<any> {
     return this.http.get<any>(
       `${this.baseUrl}/movie/${movieId}?api_key=${this.apiKey}`
     );
   }
-
+// cast and crew for a movie
   getMovieCredits(movieId: number): Observable<any> {
     return this.http.get<any>(
       `${this.baseUrl}/movie/${movieId}/credits?api_key=${this.apiKey}`
     );
   }
+  // Get details of actor/crew
   getPersonDetails(personId: number) {
   return this.http.get(`${this.baseUrl}/person/${personId}?api_key=${this.apiKey}`);
 }
-
+// cast history
 getPersonMovieCredits(personId: number) {
   return this.http.get(`${this.baseUrl}/person/${personId}/movie_credits?api_key=${this.apiKey}`);
 }
