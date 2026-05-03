95c8edc369f269ec34632345b30dc165101ce159
SeanOMahony <g00472952@atu.ie>
2026-05-03 22:37:04 +0100

    Add no image fallback display

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+// Imports angular, router and ionic
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import {
@@ -15,9 +16,12 @@ import {
 } from '@ionic/angular/standalone';
 import { NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+// Import icons
 import { addIcons } from 'ionicons';
 import { heart } from 'ionicons/icons';
+// Import TMDB service to get movie data
 import { TmdbService } from '../services/tmdb.service';
+//import spinner component
 import { IonSpinner } from '@ionic/angular/standalone';
 
 @Component({
@@ -46,7 +50,9 @@ import { IonSpinner } from '@ionic/angular/standalone';
 })
 export class HomePage implements OnInit {
 
+  //show loading spinner
   loading: boolean = true;
+  //array to store movies
   movies: any[] = [];
   searchText: string = '';
   pageTitle: string = "Today's Trending Movies";
@@ -55,19 +61,21 @@ export class HomePage implements OnInit {
     private tmdbService: TmdbService,
     private router: Router
   ) {
+    // Add heart for favourites button
     addIcons({ heart });
   }
 
   ngOnInit(): void {
     this.loadTrendingMovies();
   }
-
+// Load trending movies from API
   loadTrendingMovies(): void {
     this.loading = true;
     this.pageTitle = "Today's Trending Movies";
 
     this.tmdbService.getTrendingMovies().subscribe({
       next: (response: any) => {
+        // Store movies from API response
         this.movies = response.results || [];
         this.loading = false;
       },
@@ -77,7 +85,7 @@ export class HomePage implements OnInit {
       }
     });
   }
-
+ // Search for movies based on user input
   onSearch(): void {
     const query = this.searchText.trim();
 
@@ -100,8 +108,13 @@ export class HomePage implements OnInit {
       }
     });
   }
-
+// Navigate to favourites page
   openFavourites(): void {
     this.router.navigate(['/favourites']);
   }
+   // Clears search and reloads trending movies
+  clearSearch(): void {
+  this.searchText = '';
+  this.loadTrendingMovies();
+}
 }
