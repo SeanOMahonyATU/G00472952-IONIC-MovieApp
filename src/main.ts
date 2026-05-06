17ef9f16530d8e53e9a5a52ae8cc987446144341
SeanOMahony <g00472952@atu.ie>
2026-05-06 21:53:32 +0100

    Improve spacing and styling

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
     provideRouter(routes),
     provideHttpClient()
   ],
 });
-  
