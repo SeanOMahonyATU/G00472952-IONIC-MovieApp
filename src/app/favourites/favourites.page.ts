881a3624dad917acb48219bfd31c52e96b817f48
SeanOMahony <g00472952@atu.ie>
2026-05-07 21:49:49 +0100

    Fix details button in favourites

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,9 +1,11 @@
+// Imports for Angular, Ionic, router for pagr navigstion snd service to mangage favourites 
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { FavouritesService } from '../services/favourites.service';
 
+//defines the page
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
@@ -12,6 +14,7 @@ import { FavouritesService } from '../services/favourites.service';
   imports: [CommonModule, IonicModule]
 })
 export class FavouritesPage implements OnInit {
+  //array to store favourites
   favouriteMovies: any[] = [];
 
   constructor(
@@ -19,27 +22,32 @@ export class FavouritesPage implements OnInit {
     private router: Router
   ) {}
 
+  //runs wihen page loads first
   ngOnInit(): void {
     this.loadFavourites();
   }
-
+//runs everytime page opened
   ionViewWillEnter(): void {
     this.loadFavourites();
   }
-
+// Load favourite movies from the service
   loadFavourites(): void {
     this.favouriteMovies = this.favouritesService.getFavourites();
   }
+// Navigate to movie details page
+ openMovieDetails(movieId: number, event?: Event): void {
+  event?.stopPropagation();
+  console.log('Favourite details clicked:', movieId);
+  this.router.navigate(['/movie-details', movieId]);
 
-  openMovieDetails(movieId: number): void {
-    this.router.navigate(['/movie', movieId]);
-  }
-
+}
+  // Remove movies from favourites
   removeFavourite(movie: any): void {
     this.favouritesService.removeFromFavourites(movie);
     this.loadFavourites();
   }
 
+  //navagate back home
   goHome(): void {
     this.router.navigate(['/home']);
   }
